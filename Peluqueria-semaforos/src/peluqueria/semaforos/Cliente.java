29fc004e7dd42fba9c649ed55f344ba5a6ef3d2f
Pablo Hernandez <pablohernandezcontacto@gmail.com>
2019-10-26 18:44:07 +0200

    update

--- Peluqueria-semaforos/src/peluqueria/semaforos/Cliente.java
+++ Peluqueria-semaforos/src/peluqueria/semaforos/Cliente.java
@@ -10,71 +10,66 @@ import java.util.concurrent.Semaphore;
  *
  * @author SaFteiNZz
  */
-public class Cliente extends Thread{
-	int id;
-	Semaphore sillasLibres;
-	Semaphore sillaCortar;
-	
-	public Cliente(int id, Semaphore sillasLibres, Semaphore sillaCortar) 
-        {
-		this.id = id;
-		this.sillasLibres = sillasLibres;
-		this.sillaCortar = sillaCortar;
-	}
-	
-	@Override
-	public void run() {
-		// Entra el cliente
-		System.out.println("CLIENTE " + id + ": Hola, tienes hueco?");
-		
-		//Comprueba si hay sillas libres
-		comprobarSillasLibres(sillasLibres, sillaCortar);
-		
-		// Cliente se va
-		System.out.println("CLIENTE " + id + ": Hasta luego");
-		System.out.println("--------------------------------");
-	}
-	
-	private void comprobarSillasLibres(Semaphore semaforoSillasLibres, Semaphore semaforoSillacortar) {
-		try {   
-                        //Comprobar si el peluquero se ha dormido
-                        if(semaforoSillasLibres.availablePermits() == Peluqueria.N && semaforoSillacortar.availablePermits() > 0) {
-                            System.out.println("CLIENTE " + id + ": Eh tu! Despierta!");                        
-                        } 
-                        //Mirar si hay sillas libres
-                        if(semaforoSillasLibres.availablePermits() > 0 ) {
-                            // Coger una silla
-                            semaforoSillasLibres.acquire();
-                            System.out.println("PELUQUERO: Si tengo hueco CLIENTE " + id);
-                            //Cortar el pelo
-                            comprobarSillacortar(semaforoSillacortar);
-                            //Liberar espacio en el semaforo
-                            semaforoSillasLibres.release();    
-                        //No hay sillas
-			} else 
-                        {
-                            System.out.println("PELUQUERO: No tengo hueco, vuelve luego CLIENTE " + id );
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void comprobarSillacortar(Semaphore semaforoSillacortar) {
-		try {
-			//Coger el permiso de cortar el pelo
-			semaforoSillacortar.acquire();
-			
-			System.out.println("- CLIENTE " + id + " se está cortando el pelo");
-                        //Cortar el pelo (5 segundos)
-			Thread.sleep(5000);
-			System.out.println("- CLIENTE " + id + " se ha cortado el pelo");
-			//Librar espacio en el semaforo de cortar el pelo
-			semaforoSillacortar.release();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
+public class Cliente extends Thread 
+{
+    int id;
+    Semaphore SillasEspera;
+    Semaphore sillaCortar;
+
+    public Cliente(int id, Semaphore SillasEspera, Semaphore sillaCortar) {
+        this.id = id;
+        this.SillasEspera = SillasEspera;
+        this.sillaCortar = sillaCortar;
+    }
+
+    @Override
+    public void run() {
+        // Entra el cliente
+        System.out.println("CLIENTE " + id + ": Hola, tienes hueco?");		
+        //Comprueba si hay sillas libres
+        comprobarSillasEspera(SillasEspera, sillaCortar);		
+        // Cliente se va
+        System.out.println("CLIENTE " + id + ": Hasta luego");
+        System.out.println("--------------------------------");
+    }
+
+    private void comprobarSillasEspera(Semaphore semaforoSillasEspera, Semaphore semaforoSillaCortar) {
+        try {   
+            //Comprobar si el peluquero se ha dormido
+            if(semaforoSillasEspera.availablePermits() == Peluqueria.N && semaforoSillaCortar.availablePermits() > 0) {
+                System.out.println("CLIENTE " + id + ": Eh tu! Despierta!");                        
+            } 
+            //Mirar si hay sillas libres
+            if(semaforoSillasEspera.availablePermits() > 0 ) {
+                // Coger una silla
+                semaforoSillasEspera.acquire();
+                System.out.println("PELUQUERO: Si tengo hueco CLIENTE " + id);
+                //Cortar el pelo
+                comprobarSillaCortar(semaforoSillaCortar);
+                //Liberar espacio en el semaforo
+                semaforoSillasEspera.release();    
+            //No hay sillas
+            } else 
+            {
+                System.out.println("PELUQUERO: No tengo hueco, vuelve luego CLIENTE " + id );
+            }
+        } catch (InterruptedException e) {
+                e.printStackTrace();
+        }
+    }
+
+    private void comprobarSillaCortar(Semaphore semaforoSillaCortar) {
+        try {
+            //Coger el permiso de cortar el pelo
+            semaforoSillaCortar.acquire();
+            System.out.println("- CLIENTE " + id + " se está cortando el pelo");
+            //Cortar el pelo (5 segundos)
+            Thread.sleep(5000);
+            System.out.println("- CLIENTE " + id + " se ha cortado el pelo");
+            //Librar espacio en el semaforo de cortar el pelo
+            semaforoSillaCortar.release();
+        } catch (InterruptedException e) {			
+            e.printStackTrace();
+        }
+    }	
 }
--- Peluqueria-semaforos/src/peluqueria/semaforos/Peluqueria.java
+++ Peluqueria-semaforos/src/peluqueria/semaforos/Peluqueria.java
@@ -11,34 +11,32 @@ import java.util.concurrent.Semaphore;
  *
  * @author SaFteiNZz
  */
-public class Peluqueria {
+public class Peluqueria 
+{
     
     public static int N = 2; //Numero de sillas en la peluqueria
     
     public static void main(String[] args) throws InterruptedException {
         //Variables
         int contador = 0; //Numero de clientes que se han pasado por la peluqueria en la sesion
-        Semaphore sillasLibres = new Semaphore(N);
+        Semaphore SillasEspera = new Semaphore(N);
         Semaphore sillaCortar = new Semaphore(1); //Solo hay 1 peluquero
-
-        
 
         //Crear 20 clientes
         for (int o = 0; o <= 20; o++)
         {
-                Cliente c = new Cliente(contador, sillasLibres, sillaCortar);
-                //Ejecutar accion del cliente
-                c.start();
+            Cliente c = new Cliente(contador, SillasEspera, sillaCortar);
+            //Ejecutar accion del cliente
+            c.start();
 
-                contador++;
+            contador++;
 
-                //Tiempo para siguiente cliente se pase por la peluqueria (3-4.5s)
-                Random r = new Random();
-                int low = 3000;
-                int high = 4500;
-                int result = r.nextInt(high-low) + low;
-                Thread.sleep(result);
+            //Tiempo para siguiente cliente se pase por la peluqueria (3-4.5s)
+            Random r = new Random();
+            int low = 3000;
+            int high = 4500;
+            int result = r.nextInt(high-low) + low;
+            Thread.sleep(result);
         }
-    }
-    
+    }  
 }
